eedc8ef36be06e4bb37feeb317d610fe4d69e397
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-21 16:56:03 +0700

    test(php54): add tests for http_response_code()

--- tests/Php54FunctionsTest.php
+++ tests/Php54FunctionsTest.php
@@ -34,6 +34,16 @@ class Php54FunctionsTest extends PHPUnit\Framework\TestCase
         \Ahc\hex2bin('yx');
     }
 
+    /** @dataProvider http_response_code_data */
+    public function test_http_response_code($message, $responseCode)
+    {
+        $this->assertEquals(
+            \http_response_code($responseCode),     # expected = native   implementation
+            \Ahc\http_response_code($responseCode), # actual   = userland implementation
+            $message
+        );
+    }
+
     public function hex2bin_data()
     {
         return [[
@@ -45,6 +55,33 @@ class Php54FunctionsTest extends PHPUnit\Framework\TestCase
         ], [
             'message'   => 'mix',
             'hexString' => 'aa61bb74cc',
+        ],
+        ];
+    }
+
+    public function http_response_code_data()
+    {
+        return [[
+            'message'      => 'null',
+            'responseCode' => null,
+        ], [
+            'message'      => 'zero',
+            'responseCode' => 0,
+        ], [
+            'message'      => 'hundred',
+            'responseCode' => 100,
+        ], [
+            'message'      => 'null',
+            'responseCode' => null,
+        ], [
+            'message'      => 'zero',
+            'responseCode' => 0,
+        ], [
+            'message'      => '4 hundred',
+            'responseCode' => 400,
+        ], [
+            'message'      => '2 hundred',
+            'responseCode' => 200,
         ],
         ];
     }
