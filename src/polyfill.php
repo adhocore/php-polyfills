3eaf620166d764991bb3387267fdeb4616c09fb3
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-10-21 11:42:55 +0700

    fix: polyfill loader

--- src/polyfill.php
+++ src/polyfill.php
@@ -2,11 +2,18 @@
 
 // Apply polyfills to global scope if required.
 
-require_once __DIR__ . '/functions.php';
+/*
+ * -----
+ * PHP5.5
+ * -----
+ */
+if (PHP_VERSION_ID < 50500) {
+require_once __DIR__ . '/php55functions.php';
 
 if (!function_exists('array_column')) {
-	function array_column($array, $columnKey, $indexKey = null)
-	{
-		return \Ahc\array_column($array, $columnKey, $indexKey);
-	}
+    function array_column($array, $columnKey, $indexKey = null)
+    {
+        return \Ahc\array_column($array, $columnKey, $indexKey);
+    }
+}
 }
